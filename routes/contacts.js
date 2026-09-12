7acfe164c1ef27d1b26873452b99f8d6a4cf979f
miguelramirez26 <26ramirez.m@gmail.com>
2026-09-13 00:57:00 +0200

    Add body parameter to PUT endpoint in Swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,7 +12,20 @@ const router = express.Router()
 router.get('/', getAllContacts)
 router.get('/:id', getContactById)
 router.post('/', createContact)
-router.put('/:id', updateContact)
+router.put('/:id', (req, res, next) => {
+  /* #swagger.parameters['body'] = {
+    in: 'body',
+    required: true,
+    schema: {
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test@test.com',
+      favoriteColor: 'test',
+      birthday: '1999-09-26'
+    }
+  } */
+  updateContact(req, res, next)
+})
 router.delete('/:id', deleteContact)
 
 module.exports = router
