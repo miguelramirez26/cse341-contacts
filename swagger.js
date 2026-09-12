1b9931e926decbaaecb03c116a97d4bbdbeeaeda
miguelramirez26 <26ramirez.m@gmail.com>
2026-09-12 23:01:08 +0200

    Add production host for Swagger

--- swagger.js
+++ swagger.js
@@ -4,6 +4,7 @@ const outputFile = './swagger.json'
 const endpointsFiles = ['./routes/*.js']
 const renderHost = process.env.RENDER_EXTERNAL_HOSTNAME
 const isProduction = Boolean(renderHost || process.env.NODE_ENV === 'production')
+const productionHost = renderHost || 'cse341-contacts.onrender.com'
 
 const document = {
   info: {
@@ -11,7 +12,7 @@ const document = {
     description: 'API for managing contacts.',
     version: '1.0.0',
   },
-  host: renderHost || 'localhost:8080',
+  host: isProduction ? productionHost : 'localhost:8080',
   schemes: isProduction ? ['https'] : ['http', 'https'],
   basePath: '/contacts',
 }
